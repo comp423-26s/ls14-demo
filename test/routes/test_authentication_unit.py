5c2a6ad9f169928706337273e68d5bfa9479027f
Kris Jordan <kris@cs.unc.edu>
2026-02-23 17:08:29 +0000

    update test for unit of get_current_user

--- test/routes/test_authentication_unit.py
+++ test/routes/test_authentication_unit.py
@@ -1,18 +1,25 @@
+from unittest.mock import MagicMock
+
 import pytest
 from fastapi import HTTPException
 
+from models import User
 from routes.authentication import get_current_user
+from services.user_service import UserService
 
 
 def test_get_current_user_unit_success():
     # Arrange
-    ...
+    authorization = ...  # TODO
+    user = User(id=1, name="Sally")
+    user_svc_mock = MagicMock(spec=UserService)
+    user_svc_mock.get.return_value = user
 
     # Act
     result = get_current_user(...)
 
     # Assert
-    ...
+    ...  # TODO
 
 
 def test_get_current_user_unit_failure():
